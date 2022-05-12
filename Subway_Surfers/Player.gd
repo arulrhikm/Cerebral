fbc30b8996213a361c66500149b99c54ee865e29
ashah2023 <102239912+ashah2023@users.noreply.github.com>
2022-05-12 07:50:15 -0400

    subway surfers so far

---
+++ Subway_Surfers/Player.gd
@@ -0,0 +1,45 @@
+extends Area2D
+
+signal hit
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	position.x = 240
+	position.y = 670
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		#velocity.x += 1
+		position.x += 20
+	if Input.is_action_pressed("move_left"):
+		#velocity.x -= 1
+		position.x -= 20
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+
+
+func _on_Player_body_entered(body):
+	hide() # Player disappears after being hit.
+	emit_signal("hit")
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
